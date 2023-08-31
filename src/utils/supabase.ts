f2f8fd9a0b0e58553460785a6b77b6fdd08587a4
Ishaan Parmar <64636222+1337-ishaan@users.noreply.github.com>
2023-08-31 22:47:52 +0530

    interim

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -3,4 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://hyzvvsmybvplbrfvlhym.supabase.co";
 const supabaseKey = process.env.REACT_APP_SUPABASE_PUBLIC_KEY!;
 console.log(process.env.REACT_APP_SUPABASE_PUBLIC_KEY, "spk");
+
 export const supabase = createClient(supabaseUrl, supabaseKey)!;
